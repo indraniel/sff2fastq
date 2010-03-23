ca9801f427b768c1b471cc2c69d7d71d934dc061
Indraniel Das <indraniel@gmail.com>
2010-03-23 13:56:42 -0500

    + license text modification (addition of Washington University in StL)

--- sff.c
+++ sff.c
@@ -2,6 +2,7 @@
 
 /*
     Copyright (C) 2009, 2010 Indraniel Das <indraniel@gmail.com>
+                             and Washington University in St. Louis
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
