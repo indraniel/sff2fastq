55171ac3fa646b9626c8682b0a252dd419c370cf
Indraniel Das <indraniel@gmail.com>
2010-03-23 10:23:38 -0500

    + addition of logic to handle displaying trimmed and non-trimmed reads

--- main.c
+++ main.c
@@ -10,21 +10,40 @@
 #define PRG_NAME "sff2fastq"
 #define FASTQ_FILENAME_MAX_LENGTH 1024
 #define SFF_FILENAME_MAX_LENGTH 1024
+#define min(a,b) ( (a) < (b) ? (a) : (b) )
+#define max(a,b) ( (a) > (b) ? (a) : (b) )
 
 /* P R O T O T Y P E S *******************************************************/
 void help_message(void);
 void version_info(void);
 void process_options(int argc, char *argv[]);
-void process_sff_to_fastq(char *sff_file, char *fastq_file);
+void process_sff_to_fastq(char *sff_file, char *fastq_file, int trim_flag);
+
 void construct_fastq_entry(FILE *fp,
                            char *name,
                            char *bases,
                            uint8_t *quality,
-                           uint32_t nbases);
+                           int nbases);
+
+void get_clip_values(sff_read_header rh,
+                     int trim_flag,
+                     int *left_clip,
+                     int *right_clip);
+
+char* get_read_bases(sff_read_data rd,
+                     int left_clip,
+                     int right_clip);
+
+uint8_t* get_read_quality_values(sff_read_data rd,
+                                int left_clip,
+                                int right_clip);
 
 /* G L O B A L S *************************************************************/
 char fastq_file[FASTQ_FILENAME_MAX_LENGTH] = { '\0' };
 char sff_file[SFF_FILENAME_MAX_LENGTH] = { '\0' };
+
+// trimming is enabled by default -- like 454 tools
+int  opt_trim  = 1;
 
 /* M A I N *******************************************************************/
 int main(int argc, char *argv[]) {
@@ -35,7 +54,7 @@ int main(int argc, char *argv[]) {
 //        printf("fastq file : %s\n", fastq_file);
 //    }
 
-    process_sff_to_fastq(sff_file, fastq_file);
+    process_sff_to_fastq(sff_file, fastq_file, opt_trim);
 
     return 0;
 }
@@ -60,7 +79,7 @@ void process_options(int argc, char *argv[]) {
     int index;
     char *opt_o_value = NULL;
 
-    while( (c = getopt(argc, argv, "hvo:")) != -1 ) {
+    while( (c = getopt(argc, argv, "hvno:")) != -1 ) {
         switch(c) {
             case 'h':
                 help_message();
@@ -72,6 +91,9 @@ void process_options(int argc, char *argv[]) {
                 break;
             case 'o':
                 opt_o_value = optarg;
+                break;
+            case 'n':
+                opt_trim = 0;    /* disable trimming -- like 454 tools */
                 break;
             case '?':
 //                if ( isprint(optopt) )
@@ -108,7 +130,7 @@ void process_options(int argc, char *argv[]) {
 }
 
 void 
-process_sff_to_fastq(char *sff_file, char *fastq_file) {
+process_sff_to_fastq(char *sff_file, char *fastq_file, int trim_flag) {
     sff_common_header h;
     sff_read_header rh;
     sff_read_data rd;
@@ -147,27 +169,29 @@ process_sff_to_fastq(char *sff_file, char *fastq_file) {
 
 //    printf("Found %d reads to process\n", h.nreads);
 
+    int left_clip = 0, right_clip = 0, nbases = 0;
     char *name;
     char *bases;
+    uint8_t *quality;
     register int i;
     int numreads = (int) h.nreads;
     for (i = 0; i < numreads; i++) {
         read_sff_read_header(sff_fp, &rh);
         read_sff_read_data(sff_fp, &rd, h.flow_len, rh.nbases);
 
+        /* get clipping points */
+        get_clip_values(rh, trim_flag, &left_clip, &right_clip);
+        nbases = right_clip - left_clip;
+
         /* create bases string */
-        int bases_length = (int) rh.nbases + 1; // account for NULL termination
-        bases = (char *) malloc( bases_length * sizeof(char) );
-        if (!bases) {
-            printf("Out of memory! For bases string!\n");
-            exit(1);
-        }
-        memset(bases, '\0', (size_t) bases_length);
-        strncpy(bases, rd.bases, (size_t) rh.nbases);
+        bases = get_read_bases(rd, left_clip, right_clip);
+
+        /* create quality array */
+        quality = get_read_quality_values(rd, left_clip, right_clip);
 
         /* create read name string */
         int name_length = (int) rh.name_len + 1; // account for NULL termination
-        name = (char *) malloc( bases_length * sizeof(char) );
+        name = (char *) malloc( name_length * sizeof(char) );
         if (!name) {
             printf("Out of memory! For name string!\n");
             exit(1);
@@ -177,10 +201,11 @@ process_sff_to_fastq(char *sff_file, char *fastq_file) {
 
 //        printf("[%d | %d] %s\n", (i+1), numreads, name);
 
-        construct_fastq_entry(fastq_fp, name, bases, rd.quality, rh.nbases);
+        construct_fastq_entry(fastq_fp, name, bases, quality, nbases);
 
         free(name);
         free(bases);
+        free(quality);
         free_sff_read_header(&rh);
         free_sff_read_data(&rd);
     }
@@ -190,12 +215,97 @@ process_sff_to_fastq(char *sff_file, char *fastq_file) {
     fclose(sff_fp);
 }
 
+/* as described in section 13.3.8.2 "Read Header Section" in the
+   454 Genome Sequencer Data Analysis Software Manual
+   see (http://sequence.otago.ac.nz/download/GS_FLX_Software_Manual.pdf) */
+
+void get_clip_values(sff_read_header rh,
+                     int trim_flag,
+                     int *left_clip,
+                     int *right_clip) {
+    if (trim_flag) {
+        (*left_clip)  =
+            (int) max(1, max(rh.clip_qual_left, rh.clip_adapter_left));
+
+        // account for the 1-based index value
+        *left_clip = *left_clip - 1;
+
+        (*right_clip) = (int) min(
+              (rh.clip_qual_right    == 0 ? rh.nbases : rh.clip_qual_right   ),
+              (rh.clip_adapter_right == 0 ? rh.nbases : rh.clip_adapter_right)
+        );
+    }
+    else {
+        (*left_clip)  = 0;
+        (*right_clip) = (int) rh.nbases;
+    }
+}
+
+char* get_read_bases(sff_read_data rd,
+                     int left_clip,
+                     int right_clip) {
+    char *bases;
+
+    // account for NULL termination
+    int bases_length = (right_clip - left_clip) + 1;
+
+    // inititalize the bases string/array
+    bases = (char *) malloc( bases_length * sizeof(char) );
+    if (!bases) {
+        printf("Out of memory! For bases string!\n");
+        exit(1);
+    }
+    memset(bases, '\0', (size_t) bases_length);
+
+    // copy the relative substring
+    int start = left_clip;
+    int stop  = right_clip;
+    int i, j = 0;
+
+    for (i = start; i < stop; i++) {
+        *(bases + j) = *(rd.bases + i);
+        j++;
+    }
+
+    return bases;
+}
+
+uint8_t* get_read_quality_values(sff_read_data rd,
+                                 int left_clip,
+                                 int right_clip) {
+    uint8_t* quality;
+
+    // account for NULL termination
+    int quality_length = (right_clip - left_clip) + 1;
+
+    // inititalize the quality array
+    quality = (uint8_t *) malloc( quality_length * sizeof(uint8_t) );
+    if (!quality) {
+        printf("Out of memory! For quality array!\n");
+        printf("left clip: %d right clip: %d\n", left_clip, right_clip);
+        exit(1);
+    }
+    memset(quality, '\0', (size_t) quality_length);
+
+    // copy the relative substring
+    int start = left_clip;
+    int stop  = right_clip;
+    int i, j = 0;
+
+    for (i = start; i < stop; i++) {
+        *(quality + j) = *(rd.quality + i);
+        j++;
+    }
+
+    return quality;
+}
+
 void construct_fastq_entry(FILE *fp,
                            char *name,
                            char *bases,
                            uint8_t *quality,
-                           uint32_t nbases) {
-    register unsigned int j;
+                           int nbases) {
+    register int j;
     uint8_t quality_char;
 
     /* print out the name/sequence blocks */
