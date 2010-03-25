a497c2563d8fec7d83c1539773213b459eec8559
Indraniel Das <indraniel@gmail.com>
2010-03-25 08:29:20 -0500

    + update the program version number to 0.90

--- main.c
+++ main.c
@@ -26,7 +26,7 @@
 #include "sff.h"
 
 /* D E F I N E S *************************************************************/
-#define VERSION "0.01"
+#define VERSION "0.90"
 #define PRG_NAME "sff2fastq"
 #define FASTQ_FILENAME_MAX_LENGTH 1024
 #define SFF_FILENAME_MAX_LENGTH 1024
