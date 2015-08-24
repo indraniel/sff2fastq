dce06d240fa25ac7f6a96c6572d7cac35e89390d
indraniel <indraniel@gmail.com>
2015-08-24 12:01:28 -0500

    + update the version to 0.9.2
      - this version changes addresses Mac OS X (version 10.10) endian setup issue.

--- main.c
+++ main.c
@@ -26,7 +26,7 @@
 #include "sff.h"
 
 /* D E F I N E S *************************************************************/
-#define VERSION "0.9.1"
+#define VERSION "0.9.2"
 #define PRG_NAME "sff2fastq"
 #define FASTQ_FILENAME_MAX_LENGTH 1024
 #define SFF_FILENAME_MAX_LENGTH 1024
